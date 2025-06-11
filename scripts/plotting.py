9afd6a21390f2d3e0a04fcd330650d0f8a98f515
planT-444 <williamhu03@gmail.com>
2025-06-11 10:50:59 -0400

    we should be chillin

--- scripts/plotting.py
+++ scripts/plotting.py
@@ -4,13 +4,13 @@ import statistics
 from pathlib import Path
 import matplotlib.pyplot as plt
 
-def get_dataset(data_type, sub_type=None, outlier_names=[]):
+def get_dataset(data_type, sub_type=None, outliers=()):
     return [
         all_player_data[player_name][data_type] 
             if sub_type is None
             else all_player_data[player_name][data_type][sub_type]
         for player_name in all_player_data 
-            if player_name not in outlier_names
+            if player_name not in outliers
     ]
 
 def scatterplot(
@@ -22,7 +22,8 @@ def scatterplot(
         data_type_indep,
         sub_type_dep=None,
         sub_type_indep=None,
-        outliers=[]):
+        outliers=[],
+        fig_num=None):
     fig, plot = plt.subplots()
     dep_dataset = get_dataset(data_type_dep, sub_type_dep, outliers)
     indep_dataset = get_dataset(data_type_indep, sub_type_indep, outliers)
@@ -39,7 +40,34 @@ def scatterplot(
     plot.set_ylabel(axis_dep)
     plot.legend()
     fig.savefig(graphs_dir / f"{filename}.png")
+
+
+def boxplot(filename, title, labels, data_types, sub_types=None, tick_size=None, fig_num=None):
+    fig, plot = plt.subplots()
+    fig.set_figheight(1.5 + 0.5 * len(data_types))
+    plt.subplots_adjust(bottom=0.25)
+    if fig_num is not None:
+        print("yeet")
+        plot.text(0.5, -0.30, f"Figure {fig_num}", ha="center", color="black", fontsize=10, transform=plot.transAxes)
+
+    sub_type = [None] * len(data_types) if sub_types is None else sub_types
+    datasets = [get_dataset(data_types[i], sub_type[i]) for i in range(len(data_types))]
+    plot.boxplot(datasets, vert=False, patch_artist=True, widths=0.5)
+    plot.set_yticklabels(labels)
+    plot.set_title(title)
+    plot.grid(True)
     
+    if tick_size is not None and isinstance(tick_size, int):
+        tick_start = (
+            min(min(dataset) for dataset in datasets) // tick_size) * tick_size      
+        tick_end = ((max(max(dataset for dataset in datasets)) // tick_size) + 1) * tick_size
+        plot.set_xticks(range(tick_start, tick_end + 1, tick_size))
+        minor_tick_size = tick_size // 4
+        plot.set_xticks(range(tick_start, tick_end + 1, minor_tick_size), minor=True)
+        plot.grid(which='minor', color='gray', linestyle='--', linewidth=0.5)
+    fig.savefig(graphs_dir / f"{filename}.png")
+
+
 
 root_dir = Path(__file__).parent.parent
 graphs_dir = root_dir / "output-files" / "graphs"
@@ -53,51 +81,58 @@ with open(player_ratings_filepath, 'r') as f:
 
 print(all_player_data['Carlsen,M'].keys())
 
-win_rate_stdev = [v["win_rate_stdev"] for v in all_player_data.values()]
-rating = [v["rating"] for v in all_player_data.values()]
-avg_moves = [v["avg_moves"] for v in all_player_data.values()]
+# scatterplot(
+#     "test2",
+#     "Rating vs. Win rate",
+#     "Rating (Classical + Rapid + Blitz)",
+#     "Win rate",
+#     "rating",
+#     "win_rate",
+#     sub_type_indep="all"
+# )
 
+# scatterplot(
+#     "test2",
+#     "Rating vs. draw rate",
+#     "Rating (Classical + Rapid + Blitz)",
+#     "draw rate",
+#     "rating",
+#     "draw_rate",
+#     sub_type_indep="all"
+# )
 
+# scatterplot(
+#     "test2",
+#     "e",
+#     "win rate",
+#     "average move count",
+#     "win_rate",
+#     "avg_moves",
+#     sub_type_dep="all",
+#     sub_type_indep="all"
+# )
 
-# rating_vs_wrate_stdev
-rating_vs_wrate_stdev, plot = plt.subplots()
-lin_regg = statistics.linear_regression(win_rate_stdev, rating)
-plot.set_title("Rating vs. Std dev. of monthly win rate")
-plot.axline(xy1 = (0, lin_regg.intercept), slope = lin_regg.slope, label = f"r = {statistics.correlation(win_rate_stdev, rating)}")
-plot.scatter(win_rate_stdev, rating)
-plot.set_xlabel("Std deviation of monthly win rate")
-plot.set_ylabel("Rating (Classical + Rapid + Blitz)")
-plot.legend()
-rating_vs_wrate_stdev.savefig(graphs_dir / "rating_vs_wrate_stdev_woutlier.png")
-
-
-# rating_vs_wrate_stdev without outlier
-rating_vs_wrate_stdev, plot = plt.subplots()
-outliers = ("Carlsen,M", "Andreikin,D")
-adjusted_win_rate_stdev = get_dataset("win_rate_stdev", outliers)
-adjusted_rating = get_dataset("rating", outliers)
-lin_regg = statistics.linear_regression(
-    adjusted_win_rate_stdev,
-    adjusted_rating 
-)
-
-plot.set_title("Rating vs. Std dev. of monthly win rate (without Carlsen, Andreikin)")
-plot.axline(xy1 = (0, lin_regg.intercept), slope = lin_regg.slope, label = f"r = {statistics.correlation(adjusted_win_rate_stdev,
-    adjusted_rating )}")
-plot.scatter(adjusted_win_rate_stdev, adjusted_rating)
-plot.set_xlabel("Std deviation of monthly win rate")
-plot.set_ylabel("Rating (Classical + Rapid + Blitz)")
-plot.legend()
-rating_vs_wrate_stdev.savefig(graphs_dir / "rating_vs_wrate_stdev_wooutlier.png")
-
-plt.show()
-
-
+boxplot("ratings_boxplot", "Top 20 Players' Ratings", [""], ["rating"], tick_size=100, fig_num=1)
+boxplot("moves_boxplot", "Average Moves Per Game", ["draw", "loss", "win", "all"], ["avg_moves"] * 4, ["draw", "loss", "win", "all"], fig_num=2.1)
 scatterplot(
-    "test",
-    "Rating vs. Std dev of monthly win rate",
+    "rating_vs_draw_rate",
+    "Rating vs. Draw rate",
     "Rating (Classical + Rapid + Blitz)",
-    "Std deviation of monthly win rate",
+    "Draw rate",
     "rating",
-    "win_rate_stdev"
+    "draw_rate",
+
+    sub_type_indep="all"
 )
+scatterplot(
+    "win_rate_vs_avg_moves_draw",
+    "Win rate vs. Average moves to draw",
+    "Win rate",
+    "Avg moves to draw",
+    "win_rate",
+    "avg_moves",
+    sub_type_dep="all",
+    sub_type_indep="draw",
+)
+
+boxplot("avg_rating_diff", "Average Rating Difference", [""], ["avg_rating_diff"], ["all"], fig_num=2.2)
--- scripts/top_players.py
+++ scripts/top_players.py
@@ -10,6 +10,9 @@ if sheet is None:
     raise ValueError("No active sheet in workbook")
 
 rating_sums = defaultdict(int)
+classical = {}
+rapid = {}
+blitz = {}
 
 for cindex in (1, 4, 7):
     for rindex in range(2, 42):
@@ -17,10 +20,20 @@ for cindex in (1, 4, 7):
         rating = sheet.cell(row = rindex, column = cindex + 1).value
         if isinstance(rating, (float, int)):
             rating_sums[name] += int(rating)
-
+            match cindex:
+                case 1:
+                    blitz[name] = int(rating)
+                case 4:
+                    rapid[name] = int(rating)
+                case 7:
+                    classical[name] = int(rating)
 
 sorted_ratings = sorted(rating_sums.items(), key = lambda x: x[1], reverse = True)[:21]
 for player, rating_sum in sorted_ratings:
     print(f"{player}: {rating_sum}")
-with open(output_filepath, 'w') as f:
-    json.dump(dict(sorted_ratings), f, indent = 4)
+print("| yeet |")
+print("|------|")
+for player, rating_sum in sorted_ratings:
+    print(f'| {blitz[player]} |')
+# with open(output_filepath, 'w') as f:
+#     json.dump(dict(sorted_ratings), f, indent = 4)
